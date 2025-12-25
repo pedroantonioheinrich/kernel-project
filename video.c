ed77dfee31797b33c2c2b576597ea21ee031d1ff
Pedro Antônio Heinrich <pedroantonioheinrich@gmail.com>
2025-12-25 02:08:37 -0300

    Padronizado tipos em types.h e corrigido erro de definicao em video.c

--- video.c
+++ video.c
@@ -1,18 +1,20 @@
 #include "video.h"
 
-void kprint(const char *str, unsigned int line, unsigned char color) {
-    char *vidptr = (char*)0xb8000 + (line * 80 * 2);
-    unsigned int i = 0;
+void kprint(const char *str, uint32_t linha, uint8_t cor) {
+    // O endereço base da memória de vídeo VGA
+    char *vidptr = (char*)0xb8000 + (linha * 80 * 2);
+    uint32_t i = 0;
+    
     while (str[i] != '\0') {
         vidptr[i*2] = str[i];
-        vidptr[i*2+1] = color;
+        vidptr[i*2+1] = cor;
         i++;
     }
 }
 
 void kclear_screen() {
     char *vidptr = (char*)0xb8000;
-    unsigned int j = 0;
+    uint32_t j = 0;
     while (j < 80 * 25 * 2) {
         vidptr[j] = ' ';
         vidptr[j+1] = 0x07;
--- video.h
+++ video.h
@@ -1,14 +1,15 @@
+#include "types.h"
 #ifndef VIDEO_H
 #define VIDEO_H
 
-// Definições de cores (VGA)
+
+// Definições de cores usando uint8_t
 #define COLOR_BLACK 0x00
 #define COLOR_GREEN 0x0A
 #define COLOR_RED   0x0C
 #define COLOR_WHITE 0x0F
 
-// Protótipos das funções
-void kprint(const char *str, unsigned int linha, unsigned char cor);
+void kprint(const char *str, uint32_t linha, uint8_t cor);
 void kclear_screen();
 
 #endif
